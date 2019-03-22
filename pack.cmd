74f8d526e188b327466b635be5f49cfd925cdc2e
terrible-coder <ayanavade01@gmail.com>
2019-03-22 11:22:32 +0530

    Adding packing for browsers

---
+++ pack.cmd
@@ -0,0 +1,10 @@
+@echo off
+
+if not exist node_modules\browserify (
+	echo Bundler not found. Installing Browserify...
+	call npm install browserify --save-dev
+)
+
+echo Bundling for browser...
+call "node_modules/.bin/browserify" "./build/index.js" -o vc.js
+echo Done bundling.
