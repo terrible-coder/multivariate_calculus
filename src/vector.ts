9ab225d3fb75cdc2e82389c8881ef8b8ff3d0cb6
terrible-coder <ayanavade01@gmail.com>
2019-03-29 23:46:02 +0530

    trying class inheritance instead of type inheritance

--- src/vector.ts
+++ src/vector.ts
@@ -1,11 +1,10 @@
-import { matrix, Matrix } from "./matrix";
+import { Matrix } from "./matrix";
 
-export class Vector implements matrix {
-	readonly elements: number[][];
+export class Vector extends Matrix {
 	readonly dimension: number;
 	
 	constructor(list: number[]) {
-		this.elements = [list.slice()];
+		super([list.slice()]);
 		this.dimension = list.length;
 	}
 
@@ -13,7 +12,7 @@ export class Vector implements matrix {
 		return new Vector(this.elements[0]);
 	}
 
-	public get data() {
+	public get X() { 
 		return (i: number) => i>this.dimension? 0: this.elements[0][i-1];
 	}
 
@@ -25,23 +24,31 @@ export class Vector implements matrix {
 		return new Matrix([this.elements[0].concat(new Array(dim-this.dimension).fill(0))]);
 	}
 
+	general(dim:number) {
+		if(dim < this.dimension)
+		throw "Cannot reduce number of dimensions.";
+		if(dim === this.dimension)
+		return this.copy();
+		return new Vector(this.elements[0].slice().concat(new Array(dim-this.dimension).fill(0)));
+	}
+
 	public add(that: Vector) {
 		const dim = Math.max(this.dimension, that.dimension);
-		const A = new Matrix(this.toMatrix(dim).elements);
-		const B = new Matrix(that.toMatrix(dim).elements);
-		return new Vector(A.add(B).elements[0]);
+		const A = this.toMatrix(dim);
+		const B = that.toMatrix(dim);
+		return new Vector(A.add(B).data(0));
 	}
 
 	public sub(that: Vector) {
 		const dim = Math.max(this.dimension, that.dimension);
-		const A = new Matrix(this.toMatrix(dim).elements);
-		const B = new Matrix(that.toMatrix(dim).elements);
+		const A = this.toMatrix(dim);
+		const B = that.toMatrix(dim);
 		return new Vector(A.sub(B).data(0));
 	}
 
-	public mul(that: Vector): never {
-		throw new Error("Normal multiplication not defined for vectors.");
-	}
+	// public mul(that: Vector): never {
+	// 	throw new Error("Normal multiplication not defined for vectors.");
+	// }
 
 	public scale(k: number) {
 		return new Vector(this.elements[0].map(x => k*x));
@@ -49,8 +56,8 @@ export class Vector implements matrix {
 
 	public dot(that: Vector) {
 		const dim = Math.max(this.dimension, that.dimension);
-		const A = this.toMatrix(dim);
-		const B = that.toMatrix(dim);
+		const A = this.general(dim) as Matrix;
+		const B = that.general(dim) as Matrix;
 		return A.mul(Matrix.transpose(B)).elements[0][0];
 	}
 
@@ -58,14 +65,14 @@ export class Vector implements matrix {
 		if(this.dimension > 3 || that.dimension > 3)
 			throw new Error("Cross product defined only till 3 dimensions.");
 		const A = new Matrix([
-			[0, -this.data(3), this.data(3)],
-			[this.data(3), 0, -this.data(1)],
-			[-this.data(2), this.data(1), 0]
+			[0, -this.X(3), this.X(3)],
+			[this.X(3), 0, -this.X(1)],
+			[-this.X(2), this.X(1), 0]
 		]);
 		const B = new Matrix([
-			[that.data(1)],
-			[that.data(2)],
-			[that.data(3)]
+			[that.X(1)],
+			[that.X(2)],
+			[that.X(3)]
 		]);
 		return new Vector(Matrix.transpose(A.mul(B)).data(0));
 	}
@@ -82,11 +89,11 @@ export class Vector implements matrix {
 		return Math.acos(v1.dot(v2)/(Vector.mag(v1)*Vector.mag(v2)));
 	}
 
-	public static unit(v: Vector) {
+	public static dir(v: Vector) {
 		return v.scale(1/Vector.mag(v));
 	}
 
 	public static directionCosines(v: Vector) {
-		return Vector.unit(v).elements[0].slice();
+		return Vector.dir(v).elements[0].slice();
 	}
 }
--- tests/vector.test.js
+++ tests/vector.test.js
@@ -4,6 +4,16 @@ describe("Vector", function() {
 	const A = new Vector([1, 1, 2]);
 	const B = new Vector([1, 1, 1]);
 	const random = new Vector([100*Math.random(), 100*Math.random(), 100*Math.random(), 100*Math.random(), 100*Math.random()]);
+	console.log(A)
+	it("Adds", function() {
+		expect(A.add(B)).toBeInstanceOf(Vector);
+		expect(A.add(B)).toEqual(new Vector([2, 2, 3]));
+	});
+	it("Calculates cross product", function() {
+		const a = new Vector([1, 0, 0]);
+		const b = new Vector([0, 1, 0]);
+		expect(a.cross(b)).toEqual(new Vector([0, 0, 1]));
+	});
 	it("Calculates magnitude", function() {
 		expect(Vector.mag(random)).toBeCloseTo(Math.sqrt(random.dot(random)));
 		expect(Vector.magSq(A)).toBe(6);
