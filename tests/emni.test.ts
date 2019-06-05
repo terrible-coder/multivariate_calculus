87949da952bcd9999900403d7a4914c49974c347
terrible-coder <ayanavade01@gmail.com>
2019-06-05 10:56:40 +0530

    adding tests for new system of operators

--- tests/emni.test.ts
+++ tests/emni.test.ts
@@ -1,4 +1,5 @@
 import { Scalar, isExpression } from "../src";
+import { UnaryOperator as UO, sin, math } from "../src/unary";
 
 const x = new Scalar.Variable();
 const y = new Scalar.Variable();
@@ -7,7 +8,13 @@ const w = x.sub(y);
 const z1 = z.at(new Map([
 	[x, new Scalar.Constant(2)]
 ]));
-
+const sinx = sin(x);
+console.log(math[UO.SIN](Math.PI/2));
+console.log(math[UO.SIN](new Scalar.Constant(Math.PI/4)));
+console.log(sinx);
+console.log(sinx.at(new Map([
+	[x, new Scalar.Constant(Math.PI/4)]
+])));
 it("bitching", function() {
 	expect(isExpression(z)).toBe(true);
 	expect(isExpression(w)).toBe(true);
