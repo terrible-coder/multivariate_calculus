7e1953aa946afd97d06520c65f77fd1efd12270c
Ayanava De <ayanavade01@gmail.com>
2020-03-19 01:59:22 +0530

    Adding the levi-civita symbol

--- src/core/math/numerical.ts
+++ src/core/math/numerical.ts
@@ -26,3 +26,25 @@ export function newton_raphson(f: (x: BigNum) => BigNum, f_: (x: BigNum) => BigN
 			return BigNum.round(X, context);
 	}
 }
+
+/**
+ * The generalised Levi-Civita symbol for \(n\) dimensions.
+ * @param args The index values for the levi-civita symbol.
+ */
+export function levicivita(...args: number[]) {
+	const n = args.length;
+	for(let i = 1; i <= n; i++)
+		if(args[i] > n)
+			throw new TypeError("Index value greater than dimension of symbol.");
+		else if(args.indexOf(i) != args.lastIndexOf(i))
+			return 0;
+	let p = 0;
+	for(let i = 1; i <= n; i++) {
+		const index = args.indexOf(i);
+		const jump = index - (i-1);
+		args.splice(index, 1);
+		args.splice(i-1, 0, i);
+		p += jump;
+	}
+	return p % 2 == 0? 1: -1;
+}
