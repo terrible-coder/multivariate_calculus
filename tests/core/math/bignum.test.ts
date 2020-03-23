54ad0d13c935c91bdeef2488f76f4c9578acba24
Ayanava De <ayanavade01@gmail.com>
2020-03-24 03:07:11 +0530

    Rearranging tests

--- tests/core/math/bignum.test.ts
+++ tests/core/math/bignum.test.ts
@@ -26,6 +26,65 @@ describe("Creates numbers", function() {
 	});
 });
 
+describe("Negates", function() {
+	it("Creates additive inverse", function() {
+		const a = new BigNum(Component.create("5"), Component.create("1"), Component.create("4"));
+		expect(a.add(a.neg)).toEqual(new BigNum(Component.create("0")));
+	});
+});
+
+describe("Conjugates", function() {
+	describe("Real", function() {
+		it("Zero", function() {
+			const a = new BigNum(Component.ZERO);
+			expect(a.conj).toEqual(a);
+		});
+
+		it("Non-zero", function() {
+			for(let i = 1; i <= 10; i++) {
+				const sa = "" + i;
+				const sna = "-" + i;
+				const a = new BigNum(Component.create(sa));
+				const na = new BigNum(Component.create(sna));
+				expect(a.conj).toEqual(a);
+				expect(na.conj).toEqual(na);
+			}
+		});
+	});
+
+	describe("Complex", function() {
+		it("Purely imaginary", function() {
+			for(let i = 1; i <= 10; i++) {
+				const sa = "" + i;
+				const sna = "-" + i;
+				const a = new BigNum(Component.ZERO, Component.create(sa));
+				const na = new BigNum(Component.ZERO, Component.create(sna));
+				expect(a.conj).toEqual(na);
+			}
+		});
+
+		it("Real and imaginary", function() {
+			const a = Component.create("5");
+			for(let i = 1; i <= 10; i++) {
+				const b = Component.create("" + i);
+				const nb = Component.create("-" + i);
+				const z = new BigNum(a, b);
+				const z_ = new BigNum(a, nb);
+				expect(z.conj).toEqual(z_);
+			}
+		});
+
+		it("Involution", function() {
+			const a = Component.create("5");
+			for(let i = 1; i <= 10; i++) {
+				const b = Component.create("" + i);
+				const z = new BigNum(a, b);
+				expect(z.conj.conj).toEqual(z);
+			}
+		});
+	});
+});
+
 describe("Adds", function() {
 	it("for 1 real", function() {
 		const a = new BigNum(Component.create("5"));
@@ -132,64 +191,5 @@ describe("Multiplies", function() {
 			const a = new BigNum(A);
 			expect(a.mul(a)).toEqual(new BigNum(Component.ONE.neg));
 		}
-	});
-});
-
-describe("Negates", function() {
-	it("Creates additive inverse", function() {
-		const a = new BigNum(Component.create("5"), Component.create("1"), Component.create("4"));
-		expect(a.add(a.neg)).toEqual(new BigNum(Component.create("0")));
-	});
-});
-
-describe("Conjugates", function() {
-	describe("Real", function() {
-		it("Zero", function() {
-			const a = new BigNum(Component.ZERO);
-			expect(a.conj).toEqual(a);
-		});
-
-		it("Non-zero", function() {
-			for(let i = 1; i <= 10; i++) {
-				const sa = "" + i;
-				const sna = "-" + i;
-				const a = new BigNum(Component.create(sa));
-				const na = new BigNum(Component.create(sna));
-				expect(a.conj).toEqual(a);
-				expect(na.conj).toEqual(na);
-			}
-		});
-	});
-
-	describe("Complex", function() {
-		it("Purely imaginary", function() {
-			for(let i = 1; i <= 10; i++) {
-				const sa = "" + i;
-				const sna = "-" + i;
-				const a = new BigNum(Component.ZERO, Component.create(sa));
-				const na = new BigNum(Component.ZERO, Component.create(sna));
-				expect(a.conj).toEqual(na);
-			}
-		});
-
-		it("Real and imaginary", function() {
-			const a = Component.create("5");
-			for(let i = 1; i <= 10; i++) {
-				const b = Component.create("" + i);
-				const nb = Component.create("-" + i);
-				const z = new BigNum(a, b);
-				const z_ = new BigNum(a, nb);
-				expect(z.conj).toEqual(z_);
-			}
-		});
-
-		it("Involution", function() {
-			const a = Component.create("5");
-			for(let i = 1; i <= 10; i++) {
-				const b = Component.create("" + i);
-				const z = new BigNum(a, b);
-				expect(z.conj.conj).toEqual(z);
-			}
-		});
 	});
 });
