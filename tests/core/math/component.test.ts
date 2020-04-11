c2cc77e58db5ba4cfd745c591c8a322fa48db46d
Ayanava De <ayanavade01@gmail.com>
2020-04-11 22:29:19 +0530

    Adding tests for arc cosine

--- tests/core/math/component.test.ts
+++ tests/core/math/component.test.ts
@@ -384,22 +384,41 @@ describe("Trigonometry", function() {
 });
 
 describe("Inverse trigonometry", function() {
-	const values = [
-		"0",
-		"0.10016742116155980",
-		"0.20135792079033080",
-		"0.30469265401539751",
-		"0.41151684606748802",
-		"0.52359877559829888",
-		"0.64350110879328439",
-		"0.77539749661075307",
-		"0.92729521800161224",
-		"1.11976951499863419"
-	];
-	describe("arcsine", function() {
+	it("arc sine", function() {
+		const values = [
+			"0",
+			"0.10016742116155980",
+			"0.20135792079033080",
+			"0.30469265401539751",
+			"0.41151684606748802",
+			"0.52359877559829888",
+			"0.64350110879328439",
+			"0.77539749661075307",
+			"0.92729521800161224",
+			"1.11976951499863419"
+		];
 		for(let i = 0; i < values.length; i++) {
 			const x = Component.create("0." + i);
 			expect(Component.asin(x)).toEqual(Component.create(values[i]));
 		}
+	});
+
+	it("arc cosine", function() {
+		const values = [
+			"1.47062890563333683",
+			"1.36943840600456583",
+			"1.26610367277949912",
+			"1.15927948072740860",
+			"1.04719755119659775",
+			"0.92729521800161224",
+			"0.79539883018414356",
+			"0.64350110879328439",
+			"0.45102681179626244"
+		];
+		for(let i = 0; i < values.length; i++) {
+			const x = Component.create("0." + (i+1));
+			expect(Component.acos(x)).toEqual(Component.create(values[i]));
+		}
+		expect(Component.acos(Component.ONE)).toEqual(Component.ZERO);
 	});
 });
