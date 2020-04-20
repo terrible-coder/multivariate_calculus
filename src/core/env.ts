e5ac68b32074decfb31762c9ab5aa0f125c9de9d
Ayanava De <ayanavade01@gmail.com>
2020-04-20 19:52:47 +0530

    Improving documentation for mathenv

--- src/core/env.ts
+++ src/core/env.ts
@@ -1,10 +1,14 @@
 import { MathContext } from "./math/context";
 
 type env = {
-    /** Specifies the precision settings and rounding algorithm to use. */
-    mode?: MathContext,
+    /**
+     * Specifies the precision settings and rounding algorithm to use
+     * for numerical operations. Any operation by default will use this value if
+     * no context settings object has been given.
+     */
+    mode: MathContext,
     /** Specifies how to interpret the coordinates of any multidimensional system. */
-    coordinate_system?: "cartesian"
+    coordinate_system: "cartesian"
 }
 
 /**
