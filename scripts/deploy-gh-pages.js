a95f27aa3a8c9a9d18ef391a0ba8be704faf9fa0
Ayanava De <ayanavade01@gmail.com>
2020-05-13 22:22:23 +0530

    add: gh-pages script

---
+++ scripts/deploy-gh-pages.js
@@ -0,0 +1,16 @@
+const ghpages = require("gh-pages");
+
+ghpages.clean();
+ghpages.publish("docs", {
+	silent: true,
+	dotfiles: true,
+	add: false,
+	user: {
+		name: "Ayanava De",
+		email: "ayanavade01@gmail.com"
+	},
+	repo: `https://${process.env.GH_TOKEN}@github.com/terrible-coder/multivariate_calculus`,
+}, err => {
+	console.log("Something went wrong.");
+	console.error(err);
+});
