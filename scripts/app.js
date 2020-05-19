e618cefa65f1e218da43387f4860c5956cd94313
Ayanava De <ayanavade01@gmail.com>
2020-05-19 10:22:21 +0530

    remove unnecessary files

--- scripts/app.js
+++
@@ -1,5 +0,0 @@
-const mcalc = require("../build/index");
-
-const keys = Object.keys(mcalc);
-for(let i = 0; i < keys.length; i++)
-	window[keys[i]] = mcalc[keys[i]];
--- scripts/benchmark.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="jest" />
-declare global {
-    export namespace jest {
-        interface Matchers<R> {
-            toTakeLessThan(time: number): CustomMatcherResult;
-        }
-    }
-}
-export declare function doNothing(): void;
