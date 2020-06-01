fc923402e33adf2d0f68f2c7a100c02b837902a1
Ayanava De <ayanavade01@gmail.com>
2020-06-01 22:40:15 +0530

    script: add documentation script

---
+++ scripts/document.js
@@ -0,0 +1,23 @@
+const typedoc = require("typedoc");
+const pkg = require("../package.json");
+
+const app = new typedoc.Application();
+app.options.addReader(new typedoc.TSConfigReader());
+
+const stable = /^\d+.\d+.\d+$/;
+let outDir;
+
+if(pkg.version.match(stable) !== null)
+	outDir = "./docs";
+else outDir = "./docs/next";
+
+app.bootstrap({
+	mode: "file",
+	logger: "none",
+	module: "CommonJS",
+	theme: "./theme"
+});
+const project = app.convert(app.expandInputFiles(["./src"]));
+
+if(project)
+	app.generateDocs(project, outDir);
