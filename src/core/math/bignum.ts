590598baf8446afb778eea7dce5d505f9a26e511
Ayanava De <ayanavade01@gmail.com>
2020-06-01 20:01:45 +0530

    doc: dynamic sized parenthesis

--- src/core/math/bignum.ts
+++ src/core/math/bignum.ts
@@ -806,8 +806,8 @@ export class BigNum extends Numerical {
 	 * 
 	 * **Method**:
 	 * 
-	 * \\[ \sinh^{-1} (a + v) = \cosh^{-1} (\frac{\alpha(\theta, a) + \beta(\theta, a)}{2})
-	 * 					+ \hat{v} \sin^{-1} (\frac{\alpha(\theta, a) - \beta(\theta, a)}{2}) \\]
+	 * \\[ \sinh^{-1} (a + v) = \cosh^{-1} \left( \frac{\alpha(\theta, a) + \beta(\theta, a)}{2} \right)
+	 * 				+ \hat{v} \sin^{-1} \left( \frac{\alpha(\theta, a) - \beta(\theta, a)}{2} \right) \\]
 	 * 
 	 * where \\( \alpha(x, y) \\) and \\( \beta(x, y) \\) are defined by the
 	 * function {@link alpha_beta}.
@@ -822,8 +822,8 @@ export class BigNum extends Numerical {
 	 * 
 	 * **Method**:
 	 * 
-	 * \\[ \sinh^{-1} (a + v) = \cosh^{-1} (\frac{\alpha(\theta, a) + \beta(\theta, a)}{2})
-	 * 					+ \hat{v} \sin^{-1} (\frac{\alpha(\theta, a) - \beta(\theta, a)}{2}) \\]
+	 * \\[ \sinh^{-1} (a + v) = \cosh^{-1} \left( \frac{\alpha(\theta, a) + \beta(\theta, a)}{2} \right)
+	 * 				+ \hat{v} \sin^{-1} \left( \frac{\alpha(\theta, a) - \beta(\theta, a)}{2} \right) \\]
 	 * 
 	 * where \\( \alpha(x, y) \\) and \\( \beta(x, y) \\) are defined by the
 	 * function {@link alpha_beta}.
