6d4619c5b0b89578cce07e4ec6b886aa97d7b85c
Ayanava De <ayanavade01@gmail.com>
2020-07-06 22:25:24 +0530

    test: atan2 method in quadrant 2

--- tests/core/math/component.test.ts
+++ tests/core/math/component.test.ts
@@ -703,6 +703,17 @@ describe("Inverse trigonometry", function() {
 					expect(Component.atan2(Y[i], X[i])).toEqual(angles[i]);
 				}
 			});
+
+			test("in Quadrant II", function() {
+				const angles = new Array(9).fill(0)
+								.map((_, i) => Component.create(`0.${i+1}`))
+								.map(x => piBy2.mul(x, ctx).add(piBy2));
+				const X = angles.map(x => Component.cos(x, ctx));
+				const Y = angles.map(x => Component.sin(x, ctx));
+				for(let i = 0; i < angles.length; i++) {
+					expect(Component.atan2(Y[i], X[i])).toEqual(angles[i]);
+				}
+			});
 		});
 	});
 });
