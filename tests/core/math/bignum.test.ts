a05e292574831fa319ab61fe9dac72c0d858ddb8
Ayanava De <ayanavade01@gmail.com>
2020-07-31 21:33:38 +0530

    test: :white_check_mark: test round floor
    Adding tests to test round FLOOR algorithm.

--- tests/core/math/bignum.test.ts
+++ tests/core/math/bignum.test.ts
@@ -312,6 +312,50 @@ describe("Rounding", function() {
 			decimal.forEach((n, i) => expect(BigNum.round(n, ctx)).toEqual(rounded[i]));
 		});
 	});
+
+	describe("round floor", function() {
+		test("rounding for real", function() {
+			const ctx: MathContext = {
+				precision: 1,
+				rounding: RoundingMode.FLOOR
+			}
+			const decimal = [
+				"5.01", "5.001", "-5.01", "-5.001", "5.0", "-5.0"
+			].map(s => BigNum.real(s));
+			const rounded = [
+				"5.0", "5.0", "-5.1", "-5.1", "5", "-5"
+			].map(s => BigNum.real(s));
+			decimal.forEach((n, i) => expect(BigNum.round(n, ctx)).toEqual(rounded[i]));
+		});
+
+		test("rounding for imaginary", function() {
+			const ctx: MathContext = {
+				precision: 1,
+				rounding: RoundingMode.FLOOR
+			}
+			const decimal = [
+				"5.01", "5.001", "-5.01", "-5.001", "5.0", "-5.0"
+			].map(s => BigNum.complex("0", s));
+			const rounded = [
+				"5.0", "5.0", "-5.1", "-5.1", "5", "-5"
+			].map(s => BigNum.complex("0", s));
+			decimal.forEach((n, i) => expect(BigNum.round(n, ctx)).toEqual(rounded[i]));
+		});
+
+		test("rounding for hyper-complex", function() {
+			const ctx: MathContext = {
+				precision: 1,
+				rounding: RoundingMode.FLOOR
+			}
+			const decimal = [
+				"5.01", "5.001", "-5.01", "-5.001", "5.0", "-5.0"
+			].map(s => BigNum.hyper(s, "0", s));
+			const rounded = [
+				"5.0", "5.0", "-5.1", "-5.1", "5", "-5"
+			].map(s => BigNum.hyper(s, "0", s));
+			decimal.forEach((n, i) => expect(BigNum.round(n, ctx)).toEqual(rounded[i]));
+		});
+	});
 });
 
 describe("Adds", function() {
