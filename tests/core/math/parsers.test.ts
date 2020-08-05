b153a38ebda8069c3bd8a17e01a68208586508ad
Ayanava De <ayanavade01@gmail.com>
2020-08-05 21:32:55 +0530

    test: rearrange tests

--- tests/core/math/parsers.test.ts
+++ tests/core/math/parsers.test.ts
@@ -1,13 +1,15 @@
 import { parseNum } from "../../../src/core/math/parsers";
 
 describe("decimal notation", function() {
-	test("exact values", function () {
-		for(let i = 1; i <= 9; i++)
-			expect(parseNum(`0.${i}`)).toEqual(["", `${i}`]);
-	});
-
-	test("with leading zeroes", function() {
-		for(let i = 1; i <= 9; i++)
-			expect(parseNum(`004.${i}`)).toEqual(["4", `${i}`]);
+	describe("positive values", function() {
+		test("exact values", function () {
+			for(let i = 1; i <= 9; i++)
+				expect(parseNum(`0.${i}`)).toEqual(["", `${i}`]);
+		});
+	
+		test("with leading zeroes", function() {
+			for(let i = 1; i <= 9; i++)
+				expect(parseNum(`004.${i}`)).toEqual(["4", `${i}`]);
+		});
 	});
 });
