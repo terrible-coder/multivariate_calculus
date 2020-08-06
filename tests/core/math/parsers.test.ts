87f450ebf1b71e580f132d0e9fa4040bca83ac13
Ayanava De <ayanavade01@gmail.com>
2020-08-06 22:12:19 +0530

    test: :white_check_mark: parse numbers in exponential form
    Adding tests for parsing negative numbers in exponential form.

--- tests/core/math/parsers.test.ts
+++ tests/core/math/parsers.test.ts
@@ -94,4 +94,62 @@ describe("Exponential notation", function() {
 			});
 		});
 	});
+
+	describe("negative values", function() {
+		describe("exact value of mantissa", function() {
+			test("positive exponent", function() {
+				for(let i = 1; i <= 9; i++) {
+					expect(parseNum(`-1.${i}e2`)).toEqual([`-1${i}0`, ""]);
+					expect(parseNum(`-1.${i}e+2`)).toEqual([`-1${i}0`, ""]);
+				}
+			});
+
+			test("negative exponent", function() {
+				for(let i = 1; i <= 9; i++)
+					expect(parseNum(`-1.${i}e-2`)).toEqual(["-", `01${i}`]);
+			});
+		});
+		
+		describe("mantissa with trailing zeros", function() {
+			test("positive exponent", function() {
+				for(let i = 1; i <= 9; i++) {
+					expect(parseNum(`-1.${i}00e2`)).toEqual([`-1${i}0`, ""]);
+					expect(parseNum(`-1.${i}00e+2`)).toEqual([`-1${i}0`, ""]);
+				}
+			});
+
+			test("negative exponent", function() {
+				for(let i = 1; i <= 9; i++)
+					expect(parseNum(`-1.${i}00e-2`)).toEqual(["-", `01${i}`]);
+			});
+		});
+		
+		describe("mantissa with leading zeros", function() {
+			test("positive exponent", function() {
+				for(let i = 1; i <= 9; i++) {
+					expect(parseNum(`-001.${i}e2`)).toEqual([`-1${i}0`, ""]);
+					expect(parseNum(`-001.${i}e+2`)).toEqual([`-1${i}0`, ""]);
+				}
+			});
+
+			test("negative exponent", function() {
+				for(let i = 1; i <= 9; i++)
+					expect(parseNum(`-001.${i}e-2`)).toEqual(["-", `01${i}`]);
+			});
+		});
+		
+		describe("exponent with leading zeros", function() {
+			test("positive exponent", function() {
+				for(let i = 1; i <= 9; i++) {
+					expect(parseNum(`-1.${i}e002`)).toEqual([`-1${i}0`, ""]);
+					expect(parseNum(`-1.${i}e+002`)).toEqual([`-1${i}0`, ""]);
+				}
+			});
+
+			test("negative exponent", function() {
+				for(let i = 1; i <= 9; i++)
+					expect(parseNum(`-1.${i}e-002`)).toEqual(["-", `01${i}`]);
+			});
+		});
+	});
 });
