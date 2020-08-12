2ef588fd6d4eeba31fd0ef41e7bf887ce0d65676
Ayanava De <ayanavade01@gmail.com>
2020-08-12 20:09:06 +0530

    test: :white_check_mark: complex power
    Adding tests for raising numbers to complex powers.

--- tests/core/math/bignum.test.ts
+++ tests/core/math/bignum.test.ts
@@ -808,6 +808,16 @@ describe("Power", function() {
 			const res = BigNum.exp(new BigNum(Component.PI.div(Component.TWO, ctx)).neg);
 			expect(i.pow(i)).toEqual(BigNum.round(res, mathenv.mode));
 		});
+
+		test("for real and imaginary parts", function() {
+			expect(BigNum.real(1).pow(BigNum.complex(1, 1))).toEqual(BigNum.real(1));
+			const ctx = MathContext.HIGH_PRECISION;
+			const i = BigNum.complex(0, 1);
+			const res = BigNum.real(2).mul(
+				BigNum.exp(i.mul(new BigNum(Component.ln2), ctx), ctx), ctx
+			);
+			expect(BigNum.real(2).pow(BigNum.complex(1, 1))).toEqual(BigNum.round(res, mathenv.mode));
+		});
 	});
 });
 
